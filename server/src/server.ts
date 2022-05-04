969ac20e42ecad9eaf2fc38dc64a49808816eb32
Jefferson Charlles <contato@jefferdeveloper.com>
2022-05-04 10:58:26 -0300

    chore:concluid server ok nodejs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 
 import express from 'express'
-
+import cors from 'cors'
 import { Routes } from './routes'
 
-
 const server = express()
+
+server.use(cors())
 server.use(express.json())
 server.use(Routes)
 
